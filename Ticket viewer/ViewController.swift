4f75d71aa481a801d7181be5272635d88245ed17
nadyapost <nadyapostr@gmail.com>
2019-06-11 20:10:45 +1000

    Initial Commit

---
+++ Ticket viewer/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Ticket viewer
+//
+//  Created by Nadya Postriganova on 11/6/19.
+//  Copyright © 2019 Nadya Postriganova. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
